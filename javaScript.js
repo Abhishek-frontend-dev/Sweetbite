509a86aec7fafe7170ef06e02fc79be57398e8df
Abhishek-frontend-dev <abhishekarya2968@gmail.com>
2025-05-13 16:15:11 +0530

    first commit

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+let icons = document.querySelector('.hamburger-menu');
+let links = document.querySelector('ul');
+let lines = document.querySelector('li')
+
+icons.addEventListener("click", function () {
+   links.classList.toggle('show');
+  
+});
